bbeb82300d4cdb1111d643125d711e497d1cbdd4
Zabir Islam <zabir.islam@tufts.edu>
2015-11-12 02:08:28 -0500

    Emptied

--- messenger.js
+++
@@ -1,7 +0,0 @@
-define(function() {
-    messenger = _.extend({}, Backbone.Events)
-    // Semantic aliases
-    messenger.when = messenger.on
-    messenger.dispatch = messenger.trigger
-    return messenger;
-})
